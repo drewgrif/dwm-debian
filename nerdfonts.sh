6f49642f33eb938c715c1fe240666891412ff9fc
drewgrif <drew@drewgriffin.com>
2023-02-17 16:07:07 -0500

    Update nerdfonts.sh

--- nerdfonts.sh
+++ nerdfonts.sh
@@ -17,7 +17,7 @@ fonts=(
 for font in ${fonts[@]}
 do
     wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/$font.zip
-    sudo unzip $font.zip -d $HOME/.local/share/fonts/$font/
+    unzip $font.zip -d $HOME/.local/share/fonts/$font/
     rm $font.zip
 done
 fc-cache
